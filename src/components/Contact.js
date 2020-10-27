a3187b1b8ed3d9e51898d58b36ca094fef584a94
Laura <laura.wujek@gmail.com>
2020-10-27 14:34:08 +0100

    usage d'un state pour changer le statut online offline

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,34 @@ import './Contact.css';
 import PropTypes from 'prop-types';
 
 
-function Contact(props) {
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online : false
+    };
+  }
+
+render() {
+  return (
+    <div className="Contact">
+    <img className="avatar" src={this.props.avatar} alt={this.props.name}/>
+    <div className ="status" onClick={event => {
+            const newStatus = !this.state.online;
+            this.setState({ online: newStatus });
+          }} >
+    <h4 className="name">{this.props.name}</h4>
+    <div className = {this.state.online ? "status-online": "status-offline"} >
+    <p className ="status-text">{this.state.online ? "Online": "Offline"}</p>
+    </div>
+    </div>
+    </div>
+  )
+}
+}
+
+
+/*function Contact(props) {
   return (
     <div className="Contact">
     <img className="avatar" src={props.avatar} alt={props.name}/>
@@ -16,6 +43,7 @@ function Contact(props) {
     </div>
   );
 }
+*/
 
 Contact.propTypes = {
   name: PropTypes.string.isRequired,
